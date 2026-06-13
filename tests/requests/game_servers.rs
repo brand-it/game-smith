93b0f6ea4586a7e3033372e22f4156094489913e
Brandt Lareau <brandt@lareaus.com>
2026-06-13 12:02:16 -0700

    style: apply cargo fmt

--- tests/requests/game_servers.rs
+++ tests/requests/game_servers.rs
@@ -134,7 +134,10 @@ async fn servers_show_renders() {
             "Server name should appear"
         );
         assert!(body.contains("740"), "App ID should appear");
-        assert!(body.contains(std::env::consts::OS), "Platform should appear");
+        assert!(
+            body.contains(std::env::consts::OS),
+            "Platform should appear"
+        );
     })
     .await;
 }
